f301dec9706a7201a19d620699adb9096393b0a0
Fan Chen <chenfan0213@gmail.com>
2020-07-18 19:14:59 -0500

    commit all

--- RocketFreeMarketAPI/Controllers/AccountsController.cs
+++ RocketFreeMarketAPI/Controllers/AccountsController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using RocketFreeMarketAPI.Infrastracture;
-using RocketFreeMarketAPI.Models;
+
 
 namespace RocketFreeMarketAPI.Controllers
 {
