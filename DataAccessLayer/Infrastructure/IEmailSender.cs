3062e96b66cf956655082eec1177bff04658210c
Fan Chen <chenfan0213@gmail.com>
2020-07-22 23:06:43 -0500

    add email confirmation

---
+++ DataAccessLayer/Infrastructure/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Infrastructure
+{
+    public interface IEmailSender
+    {
+        void SendEmailConfirmation(string email);
+    }
+}
