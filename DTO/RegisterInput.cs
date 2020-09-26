534daffe7470726798d29e3dff5ed1f6d8676fda
Fan Chen <chenfan0213@gmail.com>
2020-09-26 15:22:17 -0500

    fix register

--- DTO/RegisterInput.cs
+++ DTO/RegisterInput.cs
@@ -1,9 +1,14 @@
-﻿namespace DTO
+﻿using System;
+
+namespace DTO
 {
     public class RegisterInput
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string PhoneNumber { get; set; }
     }
 }
