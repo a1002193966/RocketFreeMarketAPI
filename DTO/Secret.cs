3ba16c7021d55ac59f9c97c694f406615afe0fa7
Fan Chen <chenfan0213@gmail.com>
2020-11-01 19:39:55 -0600

    minor fix

--- DTO/Secret.cs
+++ DTO/Secret.cs
@@ -1,24 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace DTO
 {
     public class Secret
     {
-        [Required]
         public byte[] Cipher { get; set; }
-        [Required]
         public byte[] Key { get; set; }
-        [Required]
         public byte[] IV { get; set; }
     }
 
     public class SecretSerialized
     {
-        [Required]
         public string Cipher { get; set; }
-        [Required]
         public string Key { get; set; }
-        [Required]
         public string IV { get; set; }
     }
 }
--- DTO/SmtpPackage.cs
+++ DTO/SmtpPackage.cs
@@ -1,28 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace DTO
 {
     public class SmtpPackage
     {
-        [Required]
         public string Host { get; set; }
-        [Required]
         public int Port { get; set; }
-        [Required]
         public Secret UsernamePackage { get; set; }
-        [Required]
         public Secret PasswordPackage { get; set; }
     }
 
     public class SmtpPackageSerialized
     {
-        [Required]
         public string Host { get; set; }
-        [Required]
         public int Port { get; set; }
-        [Required]
         public SecretSerialized UsernamePackage { get; set; }
-        [Required]
         public SecretSerialized PasswordPackage { get; set; }
     }
 }
